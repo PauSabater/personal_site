cb2873604472c978e782a228af3a646e78c4d482
PauSabater <pau.sabater.vilar@gmail.com>
2023-09-27 19:52:34 +0200

    test deploy

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,9 +1,9 @@
 import { useLayoutEffect } from "react"
 import { showHeader } from "../components/Header/Header.animations"
 import { FootBanner } from "../components/FootBanner/FootBanner"
+import { setPageFadeInAnimation } from "../components/App/App.animations"
 // @ts-ignore -- TODO: solve declaration file from package
 import { hasPageBeenLoaded } from "@pausabater/utils/dist/index.esm.js"
-import { setPageFadeInAnimation } from "../components/App/App.animations"
 
 export default function Contact({mode, perfMode}:{mode: string, perfMode: string}) {
 
